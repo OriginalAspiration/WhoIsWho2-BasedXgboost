e9d0efad870cf3ac93950c3a90a0f12916474d3d
qyren@bupt.edu.cn <2818620348@qq.com>
2019-11-27 14:33:41 +0800

    merge the new feature

--- get_train_data.py
+++ get_train_data.py
@@ -15,68 +15,166 @@ import multiprocessing
 from multiprocessing import Pool
 import numpy as np
 from tqdm import tqdm
-import math
+import re
+import nltk.stem.porter as pt
+from nltk.corpus import stopwords
 
-from train_model import cosVector
+def fix_name(s):
+    s = s.lower().strip()
+    x = re.split(r'[^a-z]', s)
+    set_x = set()
+    for a in x:
+        if len(a) > 0:
+            set_x.add(a)
+    x = list(set_x)
+    x.sort()
+    s = ''.join(x)
+    return s
 
-def add_variate_same_author(result, paper_info_1, paper_info_2):
-    same_author = 0
+#cnt1 = 0
+#cnt2 = 0
+
+def compress_name(name):
+    #print('name', name)
+    name = name.lower().strip()
+    x = re.split(r'[^a-z]', name)
+    word_list = [word for word in x if len(word) > 0]
+    if len(word_list) == 0:
+        return '', False
+    is_compress = False
+    if len(word_list[0]) == 1:
+        is_compress = True
     
+    compress_word_list = []
+    LEN = len(word_list)
+    for i in range(LEN):
+        if i == LEN-1:
+            compress_word_list.append(word_list[i])
+        else:
+            compress_word_list.append(word_list[i][0])
+    return '_'.join(compress_word_list), is_compress
+
+def add_variate_same_author2(result, paper_info_1, paper_info_2):
+    #global cnt1 , cnt2
+    
+    same_author3 = 0
+    is_compress_authors2 = set()
+    no_compress_authors2 = set()
     authors2 = set()
     for author_2 in paper_info_2['authors']:
-        if len(replace_str(author_2['name'])) > 0:
-            authors2.add( replace_str(author_2['name']) )
+        author_2_name = replace_str(author_2['name'])
+        if len(author_2_name) > 0:
+            authors2.add( author_2_name )
+        
+        author_2_name = fix_name(author_2['name'])
+        if len(author_2_name) > 0:
+            authors2.add( author_2_name )
+        
+        author_2_name, is_compress = compress_name(author_2['name'])
+        if len(author_2_name) > 0:
+            if is_compress:
+                is_compress_authors2.add(author_2_name)
+            else:
+                no_compress_authors2.add(author_2_name)
 
     for author_1 in paper_info_1['authors']:
-        if replace_str(author_1['name']) in authors2:
-            same_author += 1
-            
+        author_1_name, is_compress = compress_name(author_1['name'])
+        flag = False
         
-    if same_author >= 2:
+        if replace_str(author_1['name']) in authors2 or \
+            fix_name(author_1['name']) in authors2:
+            flag = True
+
+        if flag == False:
+            if is_compress:
+                if author_1_name in no_compress_authors2:
+                    flag = True
+            else:
+                if author_1_name in is_compress_authors2:
+                    flag = True
+        
+        if flag:
+            same_author3 += 1
+    
+    if same_author3 >= 2:
         result.append(1)  # has same collaborator
     else:
         result.append(0)
     # the same author may be > 1000
-    result.append( min(same_author,20)*1.0/20 )
+    result.append( min(same_author3,20)*1.0/20 )
     
 
-    if same_author == 0:
+    if same_author3 == 0:
         result.append(0)
     else:
-        precision = same_author*1.0/len(paper_info_1['authors'])
-        recall = same_author*1.0/len(paper_info_2['authors'])
+        precision = same_author3*1.0/len(paper_info_1['authors'])
+        recall = same_author3*1.0/len(paper_info_2['authors'])
         F1 = 2*precision*recall/(precision+recall)
-        
-        '''try:
-            assert precision <= 1
-            assert recall <= 1
-            assert F1 <= 1
-        except:
-            print(paper_info_1['authors'])
-            print(paper_info_2['authors'])
-            print(precision, recall, F1)
-            assert False
-        '''
-        
         result.append(F1)
 
+def compare_name(name1, name2):
+    if replace_str(name1) == replace_str(name2):
+        return True
+    if fix_name(name1) == fix_name(name2):
+        return True
+    n1, i1 = compress_name(name1)
+    n2, i2 = compress_name(name2)
 
-# 判断是否是相同组织 
-def add_variate_same_org(result, paper_info_1, paper_info_2, author_rank):
-    the_author_name = replace_str(paper_info_1['authors'][author_rank]['name'])
-    same_org = 0
-    for author_2 in paper_info_2['authors']:
-        if replace_str(author_2['name']) == the_author_name:
-            if 'org' in author_2 and 'org' in paper_info_1['authors'][author_rank] and \
-                replace_str(author_2['org']) == replace_str(paper_info_1['authors'][author_rank]['org']):
-                same_org += 1
-                
-    if same_org >= 1:
-        result.append(1)  # has same org
+    if (i1 is False or i2 is False) and n1 == n2:
+        return True
+    return False
+
+def F1_between_two_org(org1, org2):
+    list1 = re.split(r'[^a-zA-Z]', org1.lower())
+    word_set = set()
+    len1 = 0
+    xx = []
+    for word in list1:
+        pt_stem = pt_stemmer.stem(word)
+        if len(pt_stem) > 0 and  pt_stem not in stop_words:
+            xx.append(pt_stem)
+            word_set.add(pt_stem)
+            len1 += 1
+
+    list2 = re.split(r'[^a-zA-Z]', org2.lower())
+    len2 = 0
+    same_word = 0
+    yy = []
+    for word in list2:
+        pt_stem = pt_stemmer.stem(word)
+        if len(pt_stem) > 0 and  pt_stem not in stop_words:
+            yy.append(pt_stem)
+            len2 += 1
+            if pt_stem in word_set:
+                same_word += 1
+    
+    if same_word:
+        precision = same_word*1.0/len1
+        recall = same_word*1.0/len2
+        F1 = 2*precision*recall/(precision+recall)
     else:
-        result.append(0)
+        F1 = 0
+    
+    return F1
+ 
+def add_variate_same_org2(result, paper_info_1, paper_info_2, author_rank):
+    same_org = 0
+    orgs = []
+    for author_2 in paper_info_2['authors']:
+        if compare_name(author_2['name'], paper_info_1['authors'][author_rank]['name']):
+            if 'org' in author_2 and 'org' in paper_info_1['authors'][author_rank]:
+                if replace_str(author_2['org']) == replace_str(paper_info_1['authors'][author_rank]['org']):
+                    same_org = 1
+                else:
+                    same_org = max(same_org, F1_between_two_org(author_2['org'], paper_info_1['authors'][author_rank]['org']) )
+                    orgs.append(author_2['org'])
+                    '''print(author_2['org'])
+                    print(paper_info_1['authors'][author_rank]['org'])
+                    print(replace_str(author_2['org']))
+                    print(replace_str(paper_info_1['authors'][author_rank]['org']))
+                    print('=================================================')'''
 
-
+    result.append(same_org)
 
     same_org = 0
     
@@ -108,6 +206,7 @@ def add_lang_result(result, paper_id_1, paper_id_2, lang_result, out_log=True):
         f = lang_result[tup]
 
     result.append(f)
+    
     if out_log:
         t = max(0.0, -np.log(f+1e-8) )/20
         
@@ -119,42 +218,132 @@ def add_lang_result(result, paper_id_1, paper_id_2, lang_result, out_log=True):
             assert False
         result.append( t )
 
+pt_stemmer = pt.PorterStemmer()
+stop_words = set(stopwords.words('english'))
+# add punctuations
+punctuations = list(string.punctuation)
+[stop_words.add(punc) for punc in punctuations]
+# remove null
+stop_words.add("null")
+stop_words.add("journal")
+stop_words.add("univers")
+stop_words.add("laboratori")
+stop_words.add("key")
+stop_words.add("journal")
+stop_words.add("school")
+stop_words.add("institut")
+stop_words.add("state")
+stop_words.add("china")
+stop_words.add("chines")
+stop_words.add("lab")
+stop_words.add("center")
+stop_words.add("scienc")
+stop_words.add("colleg")
+stop_words.add("depart")
+stop_words.add("intern")
+stop_words.add("advanc")
 
-def add_variate_same_venue(result, paper_info_1, paper_info_2):
+def add_variate_same_venue2(result, paper_info_1, paper_info_2):
+    #global cnt1 , cnt2
+    #cnt2 += 1
     if replace_str(paper_info_1['venue']) == replace_str(paper_info_2['venue']):
         result.append(1)  # has same venue
     else:
-        result.append(0)
+        #result.append(0)
 
-def add_variate_same_keywords(result, paper_info_1, paper_info_2):
-    same_keyword = 0
+        list1 = re.split(r'[^a-zA-Z]', paper_info_1['venue'].lower())
+        word_set = set()
+        len1 = 0
+        xx = []
+        for word in list1:
+            pt_stem = pt_stemmer.stem(word)
+            if len(pt_stem) > 0 and  pt_stem not in stop_words:
+                xx.append(pt_stem)
+                word_set.add(pt_stem)
+                len1 += 1
+
+        list2 = re.split(r'[^a-zA-Z]', paper_info_2['venue'].lower())
+        len2 = 0
+        same_word = 0
+        yy = []
+        for word in list2:
+            pt_stem = pt_stemmer.stem(word)
+            if len(pt_stem) > 0 and  pt_stem not in stop_words:
+                yy.append(pt_stem)
+                len2 += 1
+                if pt_stem in word_set:
+                    same_word += 1
+        
+        if same_word:
+            precision = same_word*1.0/len1
+            recall = same_word*1.0/len2
+            F1 = 2*precision*recall/(precision+recall)
+        else:
+            F1 = 0
+            
+        '''if F1 > 0:
+            print(paper_info_1['venue'])
+            print(paper_info_2['venue'])
+            print('F1', F1)
+            print('xx', xx)
+            print('yy', yy)
+
+            #cnt1 += 1
+            #print('cnt1', cnt1, 'cnt2', cnt2)
+        else:
+            print('==='=======================')
+            print('')'''
+        
+        result.append(F1)
+
+def add_variate_same_keywords2(result, paper_info_1, paper_info_2):
+    same_keyword2 = 0
+    keyword_list1 = []
     for keyword_1 in paper_info_1['keywords']:
-        for keyword_2 in paper_info_2['keywords']:
-            if replace_str( ''.join(keyword_1) ) == replace_str( ''.join(keyword_2) ):
-                same_keyword += 1
-                
+        w = ''.join([pt_stemmer.stem( re.sub(r'[^a-zA-Z]', '', word.lower() ) ) for word in keyword_1])
+        if len(w) > 0:
+            keyword_list1.append(w)
 
-    if same_keyword >= 1:
+    keyword_list2 = []
+    for keyword_2 in paper_info_2['keywords']:
+        w = ''.join([pt_stemmer.stem( re.sub(r'[^a-zA-Z]', '', word.lower() ) ) for word in keyword_2])
+        if len(w) > 0:
+            keyword_list2.append(w)
+    
+    for w1 in keyword_list1:
+        for w2 in keyword_list2:
+            if w1 == w2:
+                same_keyword2 += 1
+
+    if same_keyword2 >= 1:
         result.append(1)  # has same keyword
     else:
         result.append(0)
-    result.append( min(5.0, same_keyword) / 5 )
+    result.append( min(5.0, same_keyword2) / 5 )
 
-    if same_keyword == 0:
+    if same_keyword2 == 0:
         result.append(0)
     else:
-        precision = same_keyword*1.0/len(paper_info_1['keywords'])
-        recall = same_keyword*1.0/len(paper_info_2['keywords'])
+        precision = same_keyword2*1.0/len(keyword_list1)
+        recall = same_keyword2*1.0/len(keyword_list2)
         F1 = 2*precision*recall/(precision+recall)
         result.append(F1)
 
-def add_variate_year(result, paper_info_1, paper_info_2):
-    if 'year' not in paper_info_1 or "year" not in paper_info_2 or paper_info_1['year'] == "" or paper_info_2['year'] == "":
-        result.append( 1 )
+def add_variate_year2(result, paper_info_1, paper_info_2):
+    if 'year' not in paper_info_1 or "year" not in paper_info_2 or paper_info_1['year'] == "" or paper_info_2['year'] == "" or paper_info_1['year'] == 0 or paper_info_2['year'] == 0:
+        result.append( 5.0/20 )
+        # t = 0, t = 1 , t > 5
+        result.append( 0 )
+        result.append( 0 )
+        result.append( 0.5 )
     else:
         try:
             t = abs(paper_info_1['year'] - paper_info_2['year'])
             result.append( min(t, 20)*1.0/20 )
+
+            result.append( t == 0 )
+            result.append( t == 1 )
+            result.append( t > 5 )
         except:
             print(paper_info_1['year'], paper_info_2['year'])
             print(type(paper_info_1['year']), type(paper_info_2['year']))
@@ -195,20 +384,11 @@ def add_variate_kdd(result, paper_id_1, paper_id_2, paper_info_1, paper_info_2, 
             break
     vector_1 = get_kdd_vector(paper_id_1, author_rank_1, kdd_data, kdd_dims)
     vector_2 = get_kdd_vector(paper_id_2, author_rank_2, kdd_data, kdd_dims)
-    
-    '''if vector_1 is None or vector_2 is None:
-        t = 0.0
-    else:
-        t = cosVector(vector_1, vector_2)'''
 
     t = np.sum((vector_1 - vector_2) ** 2)
+    if kdd_dims == 100:
+        t /= 10
     result.append(t)
-
-    '''if kdd_dims == 100:
-        sum_t_100 += t
-    else:
-        sum_t_64 += t
-    count += 1'''
 
     if math.isnan(float(t)):
         print('vector_1', vector_1)
@@ -221,15 +401,15 @@ def compare_two_paper(paper_id_1, paper_id_2, paper_info_1, paper_info_2, author
                       nltk_title, nltk_abstract, gensim_title, gensum_abstract, p2p_result,
                       kdd_data=None, kdd_data_triplet=None):
     result = []
-    add_variate_same_author(result, paper_info_1, paper_info_2)  # 3
+    add_variate_same_author2(result, paper_info_1, paper_info_2)  # 3
 
-    add_variate_same_org(result, paper_info_1, paper_info_2, author_rank)  # 3
+    add_variate_same_org2(result, paper_info_1, paper_info_2, author_rank)  # 3
     
-    add_variate_year(result, paper_info_1, paper_info_2)  # 1
+    add_variate_year2(result, paper_info_1, paper_info_2)  # 4
 
-    add_variate_same_venue(result, paper_info_1, paper_info_2) # 1
+    add_variate_same_venue2(result, paper_info_1, paper_info_2) # 1
 
-    add_variate_same_keywords(result, paper_info_1, paper_info_2) # 3
+    add_variate_same_keywords2(result, paper_info_1, paper_info_2) # 3
 
     add_lang_result(result, paper_id_1, paper_id_2, nltk_title) # 2
     add_lang_result(result, paper_id_1, paper_id_2, nltk_abstract) # 2
@@ -241,7 +421,7 @@ def compare_two_paper(paper_id_1, paper_id_2, paper_info_1, paper_info_2, author
         add_variate_kdd(result, paper_id_1, paper_id_2, paper_info_1, paper_info_2, author_rank, kdd_data, 100) # 1
     if kdd_data_triplet is not None:
         add_variate_kdd(result, paper_id_1, paper_id_2, paper_info_1, paper_info_2, author_rank, kdd_data_triplet, 64) # 1
-    # 20
+    # 23
     return result
 
 def compare_paper_with_set(id_list, unass_paper_id, train_pub, author_rank, nltk_title, nltk_abstract,
