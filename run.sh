e70a2ec8d1a49fe744b27d3fef3b69ed9bae1a01
baiwentao <bwt1997@126.com>
2019-12-05 12:44:39 +0800

    final version

--- run.sh
+++ run.sh
@@ -1,24 +1,26 @@
 echo "---------- split_data.py ----------"
 date
-python split_data.py
+#python split_data.py
 echo "---------- format_process.py ----------"
 date
-python format_process.py
+#python format_process.py
 echo "---------- train_model.py ----------"
 date
-python train_model.py
+#python train_model.py
 echo "---------- paper2paper_xgb.py ----------"
 date
-python paper2paper_xgb.py
+#python paper2paper_xgb.py
 echo "---------- stord_map.py ----------"
 date
-python store_keywords_map.py
+#python store_keywords_map.py
 echo "---------- get_train_data.py ----------"
 date
-python get_train_data.py
+#python get_train_data.py
 echo "---------- train_xgb.py ----------"
 date
-python train_xgb.py
+#python train_xgb.py
 echo "---------- script.py ----------"
 date
 python script.py
+python post_process.py
+date
